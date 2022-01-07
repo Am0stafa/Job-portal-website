f526ce287d3fee0f9460fa5f7f45cc37bbe94c85
Abdelrahman mostafa <62848968+abdom0stafa@users.noreply.github.com>
2022-01-07 04:15:36 +0200

    almost done with all the api reqs

--- SERVER/Controller.js
+++ SERVER/Controller.js
@@ -160,3 +160,317 @@ exports.AdminViewEmps = async (req, res) => {
 
 }
 
+exports.AdminViewJobs = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .execute('AdminViewJobs')
+        
+        res.status(200).json({
+            status: 'success',
+            results: data.recordset.length,
+              data: {
+                data   
+              }
+          });
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+          })
+    }
+
+}
+
+exports.AdminViewFRs = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .execute('AdminViewFRs')
+        
+        res.status(200).json({
+            status: 'success',
+            results: data.recordset.length,
+              data: {
+                data   
+              }
+          });
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+          })
+    }
+
+}
+
+exports.AddContact = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('emp_id', sql.Int, req.body.emp_id)
+                            .input('name', sql.VarChar(20), req.body.name)
+                            .input('job_title', sql.VarChar(20), req.body.job_title)
+                            .input('email', sql.VarChar(20), req.body.email)
+                            .input('mobile_number', sql.VarChar(20), req.body.mobile_number)
+                            .input('fax', sql.VarChar(20), req.body.fax)
+                            .execute('AddContact')
+        
+        res.status(200).json({
+            status: 'success',
+            results:'data inserted sucessfully in Contact_person table'
+              
+          });
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+          })
+    }
+
+}
+
+exports.AddHR = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('emp_id', sql.Int, req.body.emp_id)
+                            .input('name', sql.VarChar(20), req.body.name)
+                            .input('email', sql.VarChar(20), req.body.email)
+                            .execute('AddHR')
+        
+        res.status(200).json({
+            status: 'success',
+            results:'data inserted sucessfully in HR_Director table'
+              
+          });
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+exports.ViewProfileStatus = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('emp_id', sql.Int, req.body.emp_id)
+                            .output('status', sql.VarChar(255))
+                            .output('reason', sql.VarChar(100))
+                            .execute('ViewProfileStatus')
+        
+        res.status(200).json({
+            status: 'success',
+             data: data.output
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+exports.AddFaculty = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('job_id', sql.Int, req.body.job_id)
+                            .input('allowed_faculty',sql.VarChar(20), req.body.allowed_faculty)
+                            .execute('AddFaculty')
+        
+        res.status(200).json({
+            status: 'success',
+            results:'data inserted sucessfully in Allowed_faculties table'
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+exports.AddSemester = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('job_id', sql.Int, req.body.job_id)
+                            .input('semester',sql.Int, req.body.semester)
+                            .execute('AddSemester')
+        
+        res.status(200).json({
+            status: 'success',
+            results:'data inserted sucessfully in Required_semesters table'
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+exports.EmpViewJobs = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('emp_id', sql.Int, req.body.emp_id)
+                            .execute('EmpViewJobs')
+        
+        res.status(200).json({
+            status: 'success',
+            results: data.recordset.length,
+             data:{ 
+                    data
+             }
+             
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+exports.EmpViewApplicants = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('emp_id', sql.Int, req.body.emp_id)
+                            .input('job_id', sql.Int, req.body.job_id)
+                            .execute('EmpViewApplicants')
+        
+        res.status(200).json({
+            status: 'success',
+            results: data.recordset.length,
+             data:{ 
+                    data
+             }
+             
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+exports.AddMobile = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('sid', sql.Int, req.body.sid)
+                            .input('mobileNumber',sql.VarChar(20), req.body.mobileNumber)
+                            .execute('AddMobile')
+        
+        res.status(200).json({
+            status: 'success',
+            results:'data inserted sucessfully in Student_phoneNumber table'
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+
+exports.ViewMyStatus = async (req, res) => {
+    try{
+    
+        let pool = await sql.connect(config)
+        console.log('database connected')
+        let data =await pool
+                            .request()
+                            .input('sid', sql.Int, req.body.sid)
+                            .input('job_id', sql.Int, req.body.job_id)
+                            .output('application_status', sql.VarChar(255))
+                            .execute('ViewMyStatus')
+        
+        res.status(200).json({
+            status: 'success',
+             data: data.output
+        });
+        
+    }catch(err){
+    
+        res.status(404).json({
+            status:"faild",
+            message:err.message
+                                                                                            
+        })
+    }
+
+}
+
+
+
--- SERVER/Routes.js
+++ SERVER/Routes.js
@@ -25,9 +25,53 @@ router
   .delete(Controller.DeleteProfile)
 
 router
-.route('/AdminViewEmps')
-.get(Controller.AdminViewEmps)
+  .route('/AdminViewEmps')
+  .get(Controller.AdminViewEmps)
 
+
+router
+  .route('/AdminViewJobs')
+  .get(Controller.AdminViewJobs)
+
+router
+  .route('/AdminViewFRs')
+  .get(Controller.AdminViewFRs)
+
+router
+  .route('/AddContact')
+  .post(Controller.AddContact)
+
+router
+  .route('/AddHR')
+  .post(Controller.AddHR)
+
+router
+  .route('/ViewProfileStatus')
+  .get(Controller.ViewProfileStatus)
+  
+router
+  .route('/AddFaculty')
+  .post(Controller.AddFaculty)
+  
+router
+  .route('/AddSemester')
+  .post(Controller.AddSemester)
+  
+router
+  .route('/EmpViewJobs')
+  .get(Controller.EmpViewJobs)
+
+router
+  .route('/EmpViewApplicants')
+  .get(Controller.EmpViewApplicants)
+
+router
+  .route('/AddMobile')
+  .post(Controller.AddMobile)
+
+router
+  .route('/ViewMyStatus')
+  .get(Controller.ViewMyStatus)
 
 
 
